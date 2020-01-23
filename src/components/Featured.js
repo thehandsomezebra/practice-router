a7bce2df809f2cce25ec92bbe47895235a20139d
stephanie <stephlage@gmail.com>
2020-01-22 23:29:51 -0500

    used form with onsubmit to change the url

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const Featured = ({ match }) => {
-  let fname = match.params.fname;
-  let lname = match.params.lname;
+  // let fname = match.params.fname;
+  // let lname = match.params.lname;
+  let name = match.params.name;
   let topic = match.params.topic;
   return (
     <div className="main-content">
       <h2>
-        Featured: {fname} {lname}
+        {/* Featured: {fname} {lname} */}
+        Featured: {name}
       </h2>
       <p>
-        Introducing <strong>{fname}</strong>, a teacher who loves teaching
+        Introducing <strong>{name}</strong>, a teacher who loves teaching
         courses about <strong>{topic}</strong>!
       </p>
     </div>
